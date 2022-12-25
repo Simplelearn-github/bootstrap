1c5922e28ed0c3ef96fd55b9df033af87098de5e
NaveenKonakati <naveenkonakati143@gmail.com>
2022-12-25 21:29:38 +0530

    some changes

---
+++ src/Filter.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const Filter = () => {
+    const  Student=[
+        {
+            id:101,
+            name:"Naveen",
+            mail:"naveenkonakati143@gmail.com",
+            salary:20000
+        },
+        {
+            id:102,
+            name:"Prashanth",
+            mail:"prashu67@gmail.com",
+            salary:30000
+        },
+        {
+            id:103,
+            name:"Basheer",
+            mail:"basheermd@gmail.com",
+            salary:40000
+        },
+        {
+            id:104,
+            name:"Nirosha",
+            mail:"nirosha@gmail.com",
+            salary:50000
+        },
+        {
+            id:105,
+            name:"Muthireddy",
+            mail:"muthireddy@gmail.com",
+            salary:60000
+        }
+    ]
+    return(
+        <div>
+          
+
+
+
+
+        </div>
+    )
+}
+
+export default Filter
