550a634b796f2da7d8e8c6ec60e05567a7a64824
NaveenKonakati <naveenkonakati143@gmail.com>
2022-12-26 13:28:14 +0530

    changes

--- src/Mapping.js
+++ src/Mapping.js
@@ -5,27 +5,32 @@ export const Mapping = () => {
         {
             id:101,
             name:"Naveen",
-            mail:"naveenkonakati143@gmail.com"
+            mail:"naveenkonakati143@gmail.com",
+            salary:20000
         },
         {
             id:102,
             name:"Prashanth",
-            mail:"prashu67@gmail.com"
+            mail:"prashu67@gmail.com",
+            salary:30000
         },
         {
             id:103,
             name:"Basheer",
-            mail:"basheermd@gmail.com"
+            mail:"basheermd@gmail.com",
+            salary:40000
         },
         {
             id:104,
             name:"Nirosha",
-            mail:"nirosha@gmail.com"
+            mail:"nirosha@gmail.com",
+            salary:50000
         },
         {
             id:105,
             name:"Muthireddy",
-            mail:"muthireddy@gmail.com"
+            mail:"muthireddy@gmail.com",
+            salary:60000
         }
     ]
     return(
@@ -33,7 +38,7 @@ export const Mapping = () => {
             {
                 Student.map((stu)=>{
                     return(
-                        <h1 key={stu.id}>Id is:<b style={{color:"red"}}>{stu.id} </b>&nbsp;&nbsp;Name is <b style={{color:"red"}}>{stu.name} </b>&nbsp;&nbsp;Mail Id:<b style={{color:'red'}}>{stu.mail}</b></h1>
+                        <h1 key={stu.id}>Id is:<b style={{color:"red"}}>{stu.id} </b>&nbsp;&nbsp;Name is <b style={{color:"red"}}>{stu.name} </b>&nbsp;&nbsp;Mail Id:<b style={{color:'red'}}>{stu.mail}</b>&nbsp;&nbsp; Salary:<b style={{color:'red'}}>{stu.salary}</b></h1>
                     )
                 }
                 )
